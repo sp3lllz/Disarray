bd572379f60ec8b2cafe595fb4f9542d5b3fddef
Patch <136860095+sp3lllz@users.noreply.github.com>
2025-07-16 15:47:21 +0100

    initial xcode files

---
+++ Disarray/DisarrayApp.swift
@@ -0,0 +1,17 @@
+//
+//  DisarrayApp.swift
+//  Disarray
+//
+//  Created by Patch on 16/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct DisarrayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DisarrayTests/DisarrayTests.swift
@@ -0,0 +1,16 @@
+//
+//  DisarrayTests.swift
+//  DisarrayTests
+//
+//  Created by Patch on 16/07/2025.
+//
+
+import Testing
+
+struct DisarrayTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
